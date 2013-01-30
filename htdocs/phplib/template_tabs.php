0da3f213482226314ffdea4127c71dc1046df78f
david raistrick <draistrick@ea.com>
2013-01-30 18:52:59 -0500

    fix /

--- htdocs/phplib/template_tabs.php
+++ htdocs/phplib/template_tabs.php
@@ -1,7 +1,7 @@
 <?php require_once('Dashboard.php'); ?>
 
 <? if (!empty($tabs)) { ?>
-<div class="home"><a href="/" title="Dashboards Home"><img src="images/pointer-datepicker.png"></a></div>
+<div class="home"><a href="?" title="Dashboards Home"><img src="images/pointer-datepicker.png"></a></div>
 <ul class='tabs'>
     <?
     $get_params = array();
